d2776433d559663edb0c24ac15571939b23793c0
nekokit <neko@eroneko.eu.org>
2024-03-12 13:50:39 +0000

    fix: 修改版本

--- src/appinfo/mod.rs
+++ src/appinfo/mod.rs
@@ -33,7 +33,7 @@ impl Default for AppInfo {
             lib_version: Version {
                 major: 0,
                 minor: 1,
-                revision: 3,
+                revision: 4,
                 edition: Edition::Dev,
             },
         }
